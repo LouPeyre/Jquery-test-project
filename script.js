6e8ce53dd9311ade414e6791c8485a55e2564832
LouPeyre <louis.peyredieu@gmail.com>
2023-03-20 15:48:26 +0100

    jquery ok

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 $(function() {
     
-    var $mainMenuitems = $("#main-menu ul").children("li"),
-    totalMainMenuItems = $mainMenuitems.lenght,
-    openedIndex = -1,
+    var $mainMenuItems = $("#main-menu ul").children("li"),
+    totalMainMenuItems = $mainMenuItems.length,
+    openedIndex = 2,
+
     init = function () {
-        $mainMenuitems.children(".images").click(function () {
+        bindEvents();
+
+        if(validIndex(openedIndex)){
+            animateItem($mainMenuItems.eq(openedIndex), true, 700);
+        }
+    },
+
+    bindEvents = function(){
+        $mainMenuItems.children(".images").click(function (){
             var newIndex = $(this).parent().index(),
-            $item = $mainMenuitems.eq(newIndex),
-            $colorImage = $item.find(".color");
+            $item = $mainMenuItems.eq(newIndex);
 
-            $colorImage.animate({left:"0px"},250);
-            $item.animate({width:"420px"},250);
+            if(openedIndex == newIndex){
+                animateItem($item, false, 250);
+                openedIndex = -1;
+            }
+            else{
+                if(validIndex(newIndex)){
+                    animateItem($mainMenuItems.eq(openedIndex), false, 250);
+                    openedIndex = newIndex;
+                    animateItem($item, true, 250);
+                }
+            }
+        });
+    },
 
-            openedIndex = newIndex;
-        })
+    validIndex = function(indexToCheck){
+        return (indexToCheck >= 0)  && (indexToCheck < totalMainMenuItems);
+    },
+    
+    animateItem = function($item, toOpen, speed){
+        var $colorImage = $item.find(".color"),
+        itemParam = toOpen ? {width:"420px"}:{width:"140px"},
+        colorImageParam = toOpen ? {left:"0px"}:{left:"140px"};
+        $colorImage.animate(colorImageParam,speed);
+        $item.animate(itemParam,speed);
     };
 
     init();
     
-})
+});
